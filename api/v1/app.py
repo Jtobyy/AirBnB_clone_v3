8495ff15201cf2550e8643a4d0734a37a1667911
root <root@Botoby.localdomain>
2022-02-09 00:22:36 +0100

    Build API

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,5 +6,18 @@ default flask app
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown():
+    storage.close()
+
+if __name == "__main__":
+    if getenv("HBNB_API_HOST") != None or getenv("HBNB_API_HOST") != "":
+        app.config['HBNB_API_HOST'] = '0.0.0.0'
+    if getenv("HBNB_API_PORT") != None or getenv("HBNB_API_PORT") != "":        
+        app.config['HBNB_API_PORT'] = "5000"
+    app.run(threaded=True)

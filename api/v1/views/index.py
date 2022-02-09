f2879c6c4e01b3bc1ddece68a844c3a7e41d8492
root <root@Botoby.localdomain>
2022-02-09 01:22:41 +0100

    Remove jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,10 @@ status route returns the status of the api
 """
 
 from api.v1.views import app_views
-from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
-    return jsonify({'status': 'OK'})
+    return {
+        "status": "OK"
+    }

4ebb52aee0464acf664d64760ebb96bbca1aab61
root <root@Botoby.localdomain>
2022-02-09 01:32:31 +0100

    retrieve the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,21 @@ status route returns the status of the api
 """
 
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status')
 def status():
     return {
         "status": "OK"
+    }
+
+@app_views.route('/stats')
+def stats():
+    return {
+        "amenities": storage.count("amenities"),
+        "cities": storage.count("cities"),
+        "places": storage.count("places"),
+        "reviews": storage.count("reviews"),
+        "states": storage.count("states"),
+        "users": storage.count("users")
     }

3047ca99d41946bcc57ee82c924afd9b4b613b46
root <root@Botoby.localdomain>
2022-02-09 02:04:43 +0100

    Fix Api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,20 +6,23 @@ status route returns the status of the api
 
 from api.v1.views import app_views
 from models import storage
+from flask import jsonify
+
 
 @app_views.route('/status')
 def status():
-    return {
-        "status": "OK"
-    }
+    return jsonify(
+        status="OK"
+        )
 
-@app_views.route('/stats')
+
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    return {
-        "amenities": storage.count("amenities"),
-        "cities": storage.count("cities"),
-        "places": storage.count("places"),
-        "reviews": storage.count("reviews"),
-        "states": storage.count("states"),
-        "users": storage.count("users")
-    }
+    return jsonify(
+        amenities=storage.count("amenities"),
+        cities=storage.count("cities"),
+        places=storage.count("places"),
+        reviews=storage.count("reviews"),
+        states=storage.count("states"),
+        users=storage.count("users")
+    )
